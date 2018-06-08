9f8f7a86a10634f1e1a0504edc14e17df21b1b76
mecaputy <martina.e.caputy@gmail.com>
2018-06-07 20:22:26 -0400

    e

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,3 @@
+// This file should contain two routes 
+//a GET route with the url /api/friends to display a JSON of all possible friends
+//a POST route /api/friends - this will be used to handle incoming survey results -- this route will also be used to handle the compatibility logic
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,3 @@
+//This file should include two routes
+//one route '/survey'which should display the survey page
+//a default/catch-all route that leads to home.html
