35fc7b6c851d61c159bb2a9a6ed717133463866e
mecaputy <martina.e.caputy@gmail.com>
2018-06-08 17:40:03 -0400

    routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,12 @@
 // This file should contain two routes 
+var friendData = require("../data/friends");
 //a GET route with the url /api/friends to display a JSON of all possible friends
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
+    });
+}
 //a POST route /api/friends - this will be used to handle incoming survey results -- this route will also be used to handle the compatibility logic
+app.post("/api/friends", function(req, res){
+    friendData.push(req.body);
+})
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,15 @@
 //This file should include two routes
+var path = require("path");
 //one route '/survey'which should display the survey page
-//a default/catch-all route that leads to home.html
+module.exports = function (app) {
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+
+    });
+    // A default, catch-all route that leads to home.html which displays the home page.
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+
+    });
+
+};
